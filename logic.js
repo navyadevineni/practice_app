c79b1901e585add761286ac0073c3c7974dfde44
Navya <navyadevineni30@gmail.com>
2020-02-17 17:38:25 -0600

    changes made to js file

--- logic.js
+++ logic.js
@@ -1,12 +1,12 @@
 function logic()
 {
      const Total=parseFloat(document.querySelector('#total').value)
-     const Rate=parseFloat(document.querySelector('#field-rate').value)
-     console.log(Rate);
-     const Tip=Total*(Rate/100)
-     const TaxRate=5.5
-     const Tax=Total*(TaxRate/100)
-     const GrandTotal=Total+Tip+Tax;
+     const Rate=parseFloat(document.querySelector('#rate').value)
+     console.log(rate);
+     const tip=total*(rate/100)
+     const taxRate=5.5
+     const tax=total*(taxRate/100)
+     const GrandTotal=total+tip+tax;
      document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
      document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
      document.querySelector('#total').innerHTML=`$${GrandTotal.toFixed(2)}`
