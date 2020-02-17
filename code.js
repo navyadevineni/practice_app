a51a4b58dc36ef89c0f3a4a378406fa8ff90a514
Navya <navyadevineni30@gmail.com>
2020-02-17 12:48:05 -0600

    added few changes

--- code.js
+++ code.js
@@ -6,9 +6,9 @@ function code()
      const Tip=total*(Rate/100)
      const TaxRate=5.5
      const Tax=total*(TaxRate/100)
-     const GrandTotal=Total+Tip+Tax;
-     document.querySelector('#Tip').innerHTML=`$${Tip.toFixed(2)}`
-     document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
-     document.querySelector('#Total').innerHTML=`$${GrandTotal.toFixed(2)}`
+     const GrandTotal=total+tip+tax;
+     document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#total').innerHTML=`$${GrandTotal.toFixed(2)}`
 }
      document.querySelector('#button').addEventListener('click',code)
