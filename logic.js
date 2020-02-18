fc3cf414a6fba2b178182e58a47ed1769ae1dcaf
Navya <navyadevineni30@gmail.com>
2020-02-17 20:05:00 -0600

    added commits

--- logic.js
+++ logic.js
@@ -3,12 +3,11 @@ function logic()
      const Total=parseFloat(document.querySelector('#total').value)
      const Rate=parseFloat(document.querySelector('#rate').value)
      console.log(Rate);
-     const Tip=total*(Rate/100)
-     const taxRate=5.5
-     const Tax=Total*(taxRate/100)
+     const Tip=Total*(Rate/100)
+     const Tax=Total*(5.5/100)
      const GrandTotal=Total+Tip+Tax;
      document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
      document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
-     document.querySelector('#total').innerHTML=`$${GrandTotal.toFixed(2)}`
+     document.querySelector('#grand-total').innerHTML=`$${GrandTotal.toFixed(2)}`
 }
      document.querySelector('#btn').addEventListener('click',logic)
